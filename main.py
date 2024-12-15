8e6e27cbba4d020c0f7c0112668d2f0dba7d3d26
den2k <lopateif@bk.ru>
2024-12-15 14:16:39 +0500

    Создание mainÑ

---
+++ main.py
@@ -0,0 +1,11 @@
+import telebot
+
+API_TOKEN = '7954921965:AAGUz4SIIFiO0Y6yh04ICz0-6j3v1Vmo8es'
+bot = telebot.TeleBot(API_TOKEN)
+
+@bot.message_handler(commands = ['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Здарова ебать!\nЧе опять надо?')
+
+bot.polling(none_stop=True)
+
